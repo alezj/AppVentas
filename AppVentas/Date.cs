5f16fd23aa417abfcb4a3024557fc3de299d2d16
Alezj <thebrother2011@hotmail.com>
2022-04-23 22:35:03 -0400

    se creo la clase Date

---
+++ AppVentas/Date.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppVentas
+{
+    public class Date
+    {
+        private int _year;
+        private int _month;
+        private int _day;
+
+     
+
+        public Date(int year, int month, int day)
+        {
+            _year = year;
+            _month = CheckMonth(month);
+            _day = CheckDay(day, month, year);
+        }
+
+        private int CheckDay(int day, int month, int year)
+        {
+            if (month ==2 && day == 29 &&  IsLeapYear(year))
+            {
+                return day;
+            }
+            int[] dayPerMonth = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+            if (day >=1 && day <=dayPerMonth[month] )
+            {
+                return day;
+            }
+
+            throw new DayException("Este dia es invalido:"+ day);
+        }
+
+        private bool IsLeapYear(int year)
+        {
+            return year % 400 == 0 || year % 4 == 0 && year % 100 != 0; 
+        }
+
+        private int CheckMonth(int month)
+        {
+            if(month >= 1 && month <=12)
+            {
+                return month;
+            }
+
+            throw new MonthException($"Mes errado");
+        }
+
+        public override string ToString()
+        {
+            return $"{_day:00}/{_month:00}/{_year}";
+        }
+    }
+}
---
+++ AppVentas/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AppVentas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo!");
+            Console.WriteLine("~~~~~~~~~~~");
+            try
+            {
+                Console.WriteLine(new Date(2022, 12, 7));
+                Console.WriteLine(new Date(1974, 9, 23));
+                Console.WriteLine(new Date(1985, 11, 45));
+            }
+            catch (Exception Error )
+            {
+                Console.WriteLine(Error.Message);
+        
+            }
+        }
+    }
+}
